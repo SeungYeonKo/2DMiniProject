7d00037f1f23f3e745a5326e9ff4111ffce36974
SeungYeonKo <createNewScene479@gmail.com>
2024-04-17 18:15:34 +0900

    게임방법 플롯이미지  트리거할 때 띄우기

--- Assets/02.Scripts/UI/Notice.cs
+++ Assets/02.Scripts/UI/Notice.cs
@@ -5,22 +5,33 @@ using UnityEngine.UI;
 
 public class Notice : MonoBehaviour
 {
-    public Image HowToImage; // 인스펙터에서 직접 할당
+    public Image HowToImage; 
+    private bool _isTrigger;
 
     private void Start()
     {
-        // 시작할 때 이미지를 비활성화합니다. 이미지는 인스펙터에서 할당되어야 합니다.
-        if (HowToImage != null)
-            HowToImage.gameObject.SetActive(false);
+        _isTrigger = false;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        // 플레이어가 충돌했을 때 HowToImage를 활성화합니다.
-        if (collision.collider.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !_isTrigger)
         {
+            _isTrigger = true;
             Debug.Log("HowToPlay!");
             HowToImage.gameObject.SetActive(true);
         }
+
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && _isTrigger)
+        {
+            _isTrigger = false;
+            Debug.Log("나감!");
+            HowToImage.gameObject.SetActive(false);
+        }
+
+    }
+
 }
